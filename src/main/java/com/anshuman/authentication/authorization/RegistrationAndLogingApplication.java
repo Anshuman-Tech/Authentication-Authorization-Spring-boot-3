67cdfa1b28b5239fa776ad81072c0c9c91e7756e
Anshuman Singh <anshuman20november@gmail.com>
2023-11-14 23:34:27 +0530

    The first commit

---
+++ src/main/java/com/anshuman/authentication/authorization/RegistrationAndLogingApplication.java
@@ -0,0 +1,13 @@
+package com.anshuman.authentication.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLogingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAndLogingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshuman/authentication/authorization/RegistrationAndLogingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshuman.authentication.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLogingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
